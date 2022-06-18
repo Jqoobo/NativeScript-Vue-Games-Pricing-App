4a074b095df9742769dbf3e55f2b57f8f953b011
Kacper Pieniążek <w64885@student.wsiz.edu.pl>
2022-06-18 19:08:10 +0200

    some changes in search bar

--- app/components/Browse.vue
+++ app/components/Browse.vue
@@ -3,9 +3,8 @@
     <ActionBar class="action-bar">
       <NavigationButton visibility="hidden" />
       <GridLayout columns="50, *">
-        <Label class="action-bar-title" text="Wyszukiwarka" colSpan="2" />
-
-        <Label class="fas" text.decode="&#xf0c9;" @tap="onDrawerButtonTap" />
+        <Label class="action-bar-title" text="Gameify" colSpan="2" />
+        <!--<Label class="fas" text.decode="&#xf0c9;" @tap="onDrawerButtonTap" />-->
       </GridLayout>
     </ActionBar>
 
@@ -17,12 +16,32 @@
         keyboardType="text"
         returnKeyType="done"
         @returnPress="onReturnPress($event)"
+        @focus="onFocus($event)"
+        @textChange="onTextChange($event)"
         autocorrect="false"
         maxLength="20"
       >
       </TextField>
-      <Label class="text-red-500 font-sans" :text="textFieldValue" v-if="showComponentWhenTyped" />
-      <Label class="page__content-icon far" text.decode="&#xf1ea;" />
+      <Label
+        class="text-red-500 font-sans"
+        :text="textFieldValue"
+        v-if="showComponentWhenTyped"
+      />
+      <ListView
+        class="text-red-500 font-sans"
+        v-if="showComponentWhenTyped"
+        for="item in gameListFiltered"
+      >
+        <v-template>
+          <Button class="text-sm" :text="`${item.name} - game name`" />
+        </v-template>
+      </ListView>
+      <Label
+        class="page__content-placeholder"
+        :text="message"
+        visibility="hidden"
+      />
+      <Label class="page__content-icon fas" text.decode="&#xf002;" />
       <Label class="page__content-placeholder" :text="message" />
     </GridLayout>
   </Page>
@@ -31,13 +50,18 @@
 <script>
 import * as utils from "~/shared/utils";
 import { SelectedPageService } from "../shared/selected-page-service";
+import axios from "axios/dist/axios";
+import { textProperty } from "@nativescript/core/ui/text-base";
 
 export default {
   data() {
     return {
       textField: "",
+      textFieldValue: "",
       showComponentWhenTyped: false,
       actualSearchedValue: "",
+      gameList: [],
+      gameListFiltered: [],
     };
   },
   mounted() {
@@ -45,20 +69,57 @@ export default {
   },
   computed: {
     message() {
-      return "Wyszukiwarka";
+      return "Wyszukajta byczku";
     },
     textFieldValue() {
       return this.actualSearchedValue;
     },
   },
   methods: {
-    onDrawerButtonTap() {
-      utils.showDrawer();
-    },
+    //    onDrawerButtonTap() {
+    //      utils.showDrawer();
+    //    },
     onReturnPress(event) {
       console.log(this.textField);
-      this.showComponentWhenTyped = true;
       this.actualSearchedValue = this.textField;
+    },
+    onFocus(event) {
+      this.getDataToFilter();
+    },
+    onTextChange(event) {
+      this.filterData();
+      if (this.gameListFiltered.length != 0) {
+        this.textFieldValue = this.gameListFiltered[0].name;
+        console.log("----------------------", this.gameListFiltered[0].name);
+      } else {
+        this.textFieldValue = "brak podanej gry";
+      }
+      this.showComponentWhenTyped = true;
+    },
+    getDataToFilter() {
+      if (this.gameList.length == 0) {
+        axios
+          .get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
+          .then((response) => {
+            this.gameList = response.data.applist.apps;
+            console.log(this.gameList);
+            console.log(
+              "----------------------------------------------------------------------"
+            );
+          })
+          .catch(function (error) {
+            // TODO: CATCH ERROR WHEN SIGHT NOT AVAILABLE
+            console.log(error);
+          });
+      }
+    },
+    filterData() {
+      if (this.gameList.length != 0) {
+        this.gameListFiltered = this.gameList.filter((game) => {
+          return game.name.toLowerCase().includes(this.textField.toLowerCase());
+        });
+        console.log("gameListFiltered:\n", this.gameListFiltered);
+      }
     },
   },
 };
